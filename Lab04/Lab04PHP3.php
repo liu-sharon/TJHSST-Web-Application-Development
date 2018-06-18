457688fba772c609e4442f57f1c8624f6c5a3ccb
Sharon Liu <sharonliu138@gmail.com>
2018-06-18 09:30:19 -0400

    Scrabble word generator

---
+++ Lab04/Lab04PHP3.php
@@ -0,0 +1,23 @@
+<!-- <?php
+
+	$n1 = $_REQUEST["num1"];
+echo($n1);
+?> -->
+
+<?php
+$matches=null;
+$n1 = $_REQUEST["num1"];
+//$n1 = "test2";
+$subject = "a";
+$pattern = "/\b\w{$n1}\b/";
+$matches = array();
+$matches_length=count($matches);
+$subject = file_get_contents('https://www.tjhsst.edu/~2017sliu2/Lab04/wordsEn.txt');
+
+preg_match_all($pattern, $subject, $matches);
+//for($i=0; $i<$matches_length;$i++){
+echo '<pre>';print_r($matches); echo'</pre>';
+//}
+
+//echo $n1 + 1 ;
+?>
